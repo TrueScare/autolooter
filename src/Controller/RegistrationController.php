343bdf54bdaf42644873ebe70ac75e5f065c2a98
Liam Heuwagen <liam.heuwagen@outlook.de>
2024-05-11 12:29:14 +0200

    feat(mailing): add mailservice to handle admin notifications
    also add a default mail-sender

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,20 +6,16 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
-use App\Security\LoginAuthenticator;
 use App\Service\HeaderActionService;
+use App\Service\MailService;
 use App\Service\PaginationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
@@ -38,7 +34,7 @@ class RegistrationController extends BaseController
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $authenticator, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailService $mailService): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -59,23 +55,20 @@ class RegistrationController extends BaseController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('info.autolooter@gmail.com', 'Autolooter'))
+                    ->from($mailService->getInfoMailAddress())
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
 
-            $mailtext = 'User ' . $user->getEmail() . ', ' . $user->getUsername() . ' registerd.';
-
-            $mail = (new Email())
-                ->from('info.autolooter@gmail.com')
-                ->to('info.autolooter@gmail.com')
+            $mail = (new TemplatedEmail())
                 ->subject('New User Registered')
-                ->text($mailtext)
-                ->html($mailtext);
+                ->htmlTemplate('registration/new_user_registered.html.twig');
+            $context = $mail->getContext();
+            $context['user'] = $user;
+            $mail->context($context);
 
-            $mailer->send($mail);
+            $mailService->notifyAdmin($mail);
 
             return $this->render('registration/registration_complete.html.twig', [
                 'user' => $user,
